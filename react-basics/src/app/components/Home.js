aa3ba2155a1a97a4716b2aa8375b4a9ac8b2a8eb
Luis Carlin <luiscarlin@gmail.com>
2017-12-19 06:59:56 -0500

    Dynamic data

--- react-basics/src/app/components/Home.js
+++ react-basics/src/app/components/Home.js
@@ -1,10 +1,18 @@
 import React from "react"
 
+// you can put a one-line JS statement in {}
+// you can have code before return
+
 export class Home extends React.Component {
     render() {
+        let content = ""
+        if (true) {
+            content = <p>Hello!</p>
+        }
         return (
             <div>
                 <p>In a new component!</p>
+                { content }
             </div>
         )
     }
