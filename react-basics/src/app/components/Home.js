b246909fff16ad5fa3a23ee75477accebc6df6bb
Luis Carlin <luiscarlin@gmail.com>
2017-12-21 08:17:44 -0500

    call functions without changing state

--- react-basics/src/app/components/Home.js
+++ react-basics/src/app/components/Home.js
@@ -6,10 +6,24 @@ import PropTypes from 'prop-types'
 
 // this.props.children will render anything passed in the body of the component <Comp>body</Comp>
 export class Home extends React.Component {
+    constructor(props) {
+        super()
+        this.age = props.age
+    }
+
+    onMakeOlder() {
+        this.age += 1
+        console.log(this.age)
+    }
+
+    // when calling functions from html
+    // 1. pass a reference to the function and bind this ==> onClick={this.onMakeOlder.bind(this)}
+    // 2. Or use es6 => onClick={() => this.onMakeOlder()}
+    // 3. If you want to update the UI, use state
     render() {
         return (
             <div>
-                <p>Hello, {this.props.name}. Your age is {this.props.age}.</p>
+                <p>Hello, {this.props.name}. Your age is {this.age}.</p>
                 <p>Your address is {this.props.user.address}</p>
                 <div>
                     <h4>Hobbies</h4>
@@ -18,6 +32,7 @@ export class Home extends React.Component {
                     </ul>
                 </div>
                 {this.props.children}
+                <button onClick={() => this.onMakeOlder()} className='btn btn-primary'>Make me older</button>
             </div>
         )
     }
