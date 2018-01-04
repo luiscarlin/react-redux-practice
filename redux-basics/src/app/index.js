11fef50a077df6420f5127606080e489d4c3c579
Luis Carlin <luiscarlin@gmail.com>
2018-01-04 06:30:00 -0500

    pass object as state

--- redux-basics/src/app/index.js
+++ redux-basics/src/app/index.js
@@ -2,24 +2,42 @@ import {render} from "react-dom";
 import React from "react";
 import {createStore} from "redux";
 
+const initialState = {
+    result: 1,
+    lastValues: [],
+    username: "Max"
+}
+
 // reducer takes action and changes state
-const reducer = (state, action) => {
+// in es6, you can have default params
+const reducer = (state = initialState, action) => {
     // find which action to perform
     // convention to use UPPERCASE action types
     switch (action.type) {
         case "ADD":
-            state = state + action.payload;
+            // make a shallow copy off the top level (note this is NOT a deep copy)
+            // then override values
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            }
             break;
         case "SUBTRACT":
-            state = state - action.payload;
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            }
             break;
     }
     // the new state the app will use
     return state;
 };
 
-// pass: reducer and initial app state (can be any type)
-const store = createStore(reducer, 1);
+// pass: reducer and initial app state (can be any type. object, int, arrays)
+// if you provide a default for intial state in the reducer, you don't need to pass it
+const store = createStore(reducer);
 
 // fire this callback when the store is updated
 // for testing purposes (since we're not using react atm)
